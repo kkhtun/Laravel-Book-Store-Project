b65fdc9942a2babfbb4979f1d602ff1d432aaa44
Khaing Khant Htun <khaingkhanthtun@gmail.com>
2021-06-10 21:37:57 +0630

    adjusted col-span in welcome page for mobile view cover image display

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,10 +26,10 @@
                 @foreach($books as $book)
                 <a href="/book?id={{ $book->id }}" class="hover:no-underline hover:text-black">
                     <div class="grid grid-cols-12 border border-1 rounded-md hover:bg-gray-300 focus:bg-gray-300 h-full">
-                        <div class="col-span-3 p-2">
+                        <div class="col-span-4 sm:col-span-3 p-2">
                             <img src="/covers/{{ $book->covername }}" alt="book cover" onerror="this.src='/covers/placeholder.jpg'"  class="rounded-md img-cover">
                         </div>
-                        <div class="col-span-9 p-2 relative">
+                        <div class="col-span-8 sm:col-span-9 p-2 relative">
                             <h2 class="font-bold text-lg">{{ $book->name }}</h2>
                             <p class="text-muted text-xs mt-1">Author: {{ $book->author }}</p>
                             <p class="absolute bottom-2 right-2"><small class="bg-blue-500 text-white rounded-md px-3 py-1">{{ $book->category->name }}</small></p>
