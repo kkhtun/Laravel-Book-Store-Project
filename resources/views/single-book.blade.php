f77df53c2cc1d386d1b12cbf86ffe6ea2726a9e8
Khaing Khant Htun <khaingkhanthtun@gmail.com>
2021-06-14 22:24:50 +0630

    changed single page view on mobile to non-grid layout and made minor style adjustments

--- resources/views/single-book.blade.php
+++ resources/views/single-book.blade.php
@@ -7,22 +7,22 @@
             <div class="card">
                 <div class="card-header">{{ __('Book Details') }}</div>
 
-                <div class="card-body grid grid-cols-6 gap-2">
+                <div class="card-body sm:grid grid-cols-6 gap-2">
 
                     <div class="lg:col-span-1 col-span-2">
-                        <img src="/covers/{{ $book->covername }}" alt="book cover" onerror="this.src='/covers/placeholder.jpg'" class="rounded-md">
+                        <img src="/covers/{{ $book->covername }}" alt="book cover" onerror="this.src='/covers/placeholder.jpg'" class="rounded-md sm:h-auto h-48 mx-auto">
                     </div>
 
-                    <div class="lg:col-span-5 col-span-4 pl-2 relative">
+                    <div class="lg:col-span-5 col-span-4 pl-2 relative sm:mt-0 mt-4">
                             <h2 class="text-2xl">{{ $book->name }}</h2>
-                            <small class="font-semibold">{{ $book->author }}</small>  
+                            <span class="text-muted text-sm">Author: </span><small class="font-semibold">{{ $book->author }}</small>  
                             <p class="lg:absolute top-0 right-0"><small class="border border-1 bg-blue-500 text-white rounded-md px-3 lg:py-1">{{ $book->category->name }}</small></p>
 
-                            <p class="mt-3 p-2 border border-1 rounded-md">{{ $book->about }}</p>
+                            <p class="mt-3 p-2 border border-1 rounded-md sm:text-md text-sm text-justify">{{ $book->about }}</p>
                               
                             <div class="mt-4 font-semibold">   
-                                <i class="text-muted text-xs mb-2 block">Please note that some browsers may have issues opening PDF files. For better experience, we suggest downloading the file and opening it using native applications that support PDF viewing.</i>  
-                                <div>      
+                                <i class="text-muted text-xs mb-2 block text-justify pr-1">Please note that some browsers may have issues opening PDF files. For better experience, we suggest downloading the file and opening it using native applications that support PDF viewing. For Safari, directly use the download link to view the PDF.</i>  
+                                <div class="mt-2 text-md-left text-center">      
                                     <a class="btn btn-success lg:w-32 w-28 m-2" href="{{ url('/download', $book->filename) }}">Download</a>
                                     <a class="btn btn-outline-info lg:w-32 w-28 m-2" href="{{ url('/preview', $book->filename) }}" target="_blank">Preview</a>
                                 </div> 
